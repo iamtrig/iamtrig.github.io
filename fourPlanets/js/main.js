345b2401e9ddd3b2b927a81a9ddb6bebf478097c
G3 <G3@fck.local>
2016-07-27 17:39:20 +0900

    fourPlanets test

--- fourPlanets/js/main.js
+++ fourPlanets/js/main.js
@@ -8,6 +8,10 @@ jQuery( document ).ready(function( $ ) {
   $("#mainMenu").click(function(){
   	$(".navBg, .navA").toggleClass("firstHide");
 
+  	$(this).find("span:eq(0)").toggleClass("navTop");
+  	$(this).find("span:eq(1)").toggleClass("navMid");
+  	$(this).find("span:eq(2)").toggleClass("navBttm");
+
   	return false;
   });
 
