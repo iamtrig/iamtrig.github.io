db2d2c7e13716afa85dbc5046c9bc0d4b05123b3
G3 <G3@fck.local>
2016-07-30 02:47:02 +0900

    fr

---
+++ fourPlanets/js/subScript.js
@@ -0,0 +1,22 @@
+$.noConflict();
+jQuery( document ).ready(function( $ ) {
+  // Code that uses jQuery's $ can follow here.
+
+
+  // main menu 
+
+  $("#mainMenu").click(function(){
+  	$(".navBg, .navA").toggleClass("firstHide");
+
+  	$(this).find("span:eq(0)").toggleClass("navTop");
+  	$(this).find("span:eq(1)").toggleClass("navMid");
+  	$(this).find("span:eq(2)").toggleClass("navBttm");
+
+  	return false;
+  });
+
+
+
+});
+
+
