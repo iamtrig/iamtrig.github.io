f5109a14441bf88a6a538443f21a7bd6349d4661
G3 <G3@fck.local>
2016-08-04 00:47:50 +0900

    updates

--- fourPlanets/js/subScript.js
+++ fourPlanets/js/subScript.js
@@ -1,4 +1,4 @@
-$.noConflict();
+
 jQuery( document ).ready(function( $ ) {
   // Code that uses jQuery's $ can follow here.
 
@@ -30,12 +30,50 @@ jQuery( document ).ready(function( $ ) {
   // info planets
 
 
+  // $("#talk a").click(function(){
+
+  //     $(this).toggleClass("changeBg");
+
+  //     $("#talkSection, .hideRightBtm, .hideRightTop").addClass("showNow");
+
+  //     if ($(".hideRightBtm, .hideRightTop").hasClass("showNow")) {
+  //       $(".hideRightBtm").show();
+  //     } else {
+  //       $(".hideRightBtm").hide();
+  //       $(".hideRightBtm").removeClass("showNow");
+  //   }
+  //   return false;
+  // });
+  
+  var getWidth = $(window).width();
+
+  $(window).resize(function(){
+    getWidth = $(window).width();
+
+  });
+
+
   $("#talk a").click(function(){
+    $("#talkSection").toggleClass("showNow");
+    $(".hideRightBtm, .hideRightTop").toggleClass("showNow");
 
-      $(this).toggleClass("changeBg");
+    if ($(".hideRightBtm, .hideRightTop").hasClass("showNow")) {
+      $(".hideRightBtm, .hideRightTop").show();
+    } else {
+      $(".hideRightBtm, .hideRightTop").removeClass("showNow");
+      $(".hideRightBtm, .hideRightTop").hide();
+    }
 
-      $("#talkSection, .hideRightBtm, .hideRightTop").toggleClass("showNow");
+    return false;
+  });
 
+
+  $("#talkSection ul#firstUl > li:last-child a").click(function(){
+     $("#talkSection").removeClass("showNow");
+     $(".hideRightBtm, .hideRightTop").removeClass("showNow");
+
+    $(".hideRightBtm").hide();
+    $(".hideRightTop").hide();
     return false;
   });
 
